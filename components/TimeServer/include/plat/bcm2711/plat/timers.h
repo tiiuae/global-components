ab3bf0e3f19abdec373d564ebbf49ff92a40ba42
Joonas Onatsu <joonas.onatsu@unikie.com>
2021-04-07 15:30:50 +0300

    Fix timers

--- components/TimeServer/include/plat/bcm2711/plat/timers.h
+++ components/TimeServer/include/plat/bcm2711/plat/timers.h
@@ -4,22 +4,39 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 #pragma once
+        
+#define HARDWARE_TIMER_COMPONENT                                       \
+    component HWTimer {                                                \
+        hardware;                                                      \
+        dataport Buf(4096) mem;                                        \
+        emits TimerIRQ irq;                                            \
+    }
 
-/*
- *  hwtimer.mem_paddr = 0xfe003000;                                     
- *  hwtimer.mem_size = 0x1000;                                          
- *  hwtimer.irq_irq_number = 97;                                        
- *  hwtimer.irq_irq_type = "arm";                                       
- *  hwtimer.irq_irq_trigger = "edge";
- */
+#define HARDWARE_TIMER_INTERFACES                                      \
+    dataport Buf(4096) systimer_mem;                                   \
+    dataport Buf(4096) sp804_mem;                                      \
+    consumes TimerIRQ systimer_irq;                                    \
+    consumes TimerIRQ sp804_irq;                                    
 
-#define HARDWARE_TIMER_INTERFACES                                                   \
-    consumes Dummy bcm_timer;                                                       \
-    emits Dummy dummy_source;
 #define HARDWARE_TIMER_ATTRIBUTES
-#define HARDWARE_TIMER_COMPOSITION                                                  \
-        connection seL4DTBHardware bcm_timer_conn(from dummy_source, to bcm_timer);
-#define HARDWARE_TIMER_CONFIG                                                       \
-        bcm_timer.dtb = dtb({"path" : "/soc/timer@7e003000"});                      \
-        bcm_timer.generate_interrupts = 1;
+#define HARDWARE_TIMER_COMPOSITION                                       \
+        component HWTimer systimer;                                      \
+        connection seL4HardwareMMIO systimer_mem(from systimer_mem,      \
+                                                 to systimer.mem);       \
+        connection seL4HardwareInterrupt systimer_irq(from systimer.irq, \
+                                                      to systimer_irq);  \
+        component HWTimer sp804timer;                                    \
+        connection seL4HardwareMMIO sp804_mem(from sp804_mem,            \
+                                                 to sp804timer.mem);     \
+        connection seL4HardwareInterrupt sp804_irq(from sp804timer.irq,  \
+                                          ¸            to sp804_irq);
+#define HARDWARE_TIMER_CONFIG                                            \
+    systimer.mem_paddr = 0xfe003000;                                     \
+    systimer.mem_size = 0x1000;                                          \
+    systimer.irq_irq_number = 97;                                        \
+    systimer.irq_irq_type = "arm";                                       \
+    sp804timer.mem_paddr = 0xfe00b000;                                   \
+    sp804timer.mem_size = 0x1000;                                        \
+    sp804timer.irq_irq_number = 64;                                      \
+    sp804timer.irq_irq_type = "arm";                                     
 #define HARDWARE_TIMER_PLAT_INTERFACES
